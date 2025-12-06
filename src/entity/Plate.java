a4f73da07af7b76e7ac71999d22f726fa004ad9b
Andreas <andreast4mbun@gmail.com>
2025-12-07 03:31:06 +0700

    implement Plate and FryingPan

---
+++ src/entity/Plate.java
@@ -0,0 +1,31 @@
+package entity;
+
+public class Plate extends KitchenUtensil {    
+    private boolean isClean;
+
+    public Plate() {
+        // memanggil konstruktor KitchenUtensil
+        super();
+        // defaultnya piring bersih saat diambil
+        this.isClean = true; 
+    }
+
+    // getter untuk cek kebersihan
+    public boolean isClean() {
+        return isClean;
+    }
+
+    // method untuk mencuci piring (dipanggil di Washing Station)
+    public void wash() {
+        this.isClean = true;
+        System.out.println("Plate is now clean.");
+    }
+
+    // method untuk membuat piring kotor (dipanggil setelah makan atau serving)
+    public void dirty() {
+        this.isClean = false;
+        // saat kotor, isinya harus dibuang/kosong
+        this.contents.clear(); 
+        System.out.println("Plate is now dirty.");
+    }
+}
