3387f5ac1994e04e79427ff2c7721e2d3ba62218
herse <herseprabowo@gmail.com>
2025-12-06 12:46:00 +0700

    feat(ingredients): implement 5 items for Map C Burger
    - Added Daging, Roti, Keju, Lettuce, and Tomat classes
    - Refactored Ingredients class to support state changes
    - Updated InterfaceChopable logic

--- src/entity/item/ItemFactory.java
+++ src/entity/item/ItemFactory.java
@@ -2,41 +2,41 @@ package entity.item;
 
 public class ItemFactory {
     
-    public static Item createItem(String itemType) {
-        switch(itemType.toLowerCase()) {
-            // Ingredients
-            case "tomato":
-                return new Tomato();
-            case "lettuce":
-                return new Lettuce();
-            case "onion":
-                return new Onion();
-            case "meat":
-                return new Meat();
+    // public static Item createItem(String itemType) {
+    //     switch(itemType.toLowerCase()) {
+    //         // Ingredients
+    //         case "tomato":
+    //             return new Tomato();
+    //         case "lettuce":
+    //             return new Lettuce();
+    //         case "onion":
+    //             return new Onion();
+    //         case "meat":
+    //             return new Meat();
                 
-            // Dishes
-            case "salad":
-                return new Salad();
-            case "burger":
-                return new Burger();
+    //         // Dishes
+    //         case "salad":
+    //             return new Salad();
+    //         case "burger":
+    //             return new Burger();
                 
-            // Kitchen Utensils
-            case "knife":
-                return new Knife();
-            case "plate":
-                return new Plate();
-            case "pan":
-                return new Pan();
+    //         // Kitchen Utensils
+    //         case "knife":
+    //             return new Knife();
+    //         case "plate":
+    //             return new Plate();
+    //         case "pan":
+    //             return new Pan();
                 
-            default:
-                throw new IllegalArgumentException("Unknown item type: " + itemType);
-        }
-    }
+    //         default:
+    //             throw new IllegalArgumentException("Unknown item type: " + itemType);
+    //     }
+    // }
     
     // Overload for items with position
-    public static Item createItem(String itemType, int x, int y) {
-        Item item = createItem(itemType);
-        item.setPosition(x, y);
-        return item;
-    }
+    // public static Item createItem(String itemType, int x, int y) {
+    //     Item item = createItem(itemType);
+    //     item.setPosition(x, y);
+    //     return item;
+    // }
 }
---
+++ src/entity/item/Keju.java
@@ -0,0 +1,45 @@
+package entity.item;
+
+public class Keju extends Ingredients implements InterfaceChopable, InterfacePlatable {
+    
+    private boolean isChopped = false;
+
+    public Keju() {
+        this.name = "Keju";
+        this.raw = true;
+    }
+
+    @Override
+    public void use() { System.out.println("Memegang Keju"); }
+
+    @Override
+    public void canBeChopped() {
+    }
+
+    @Override
+    public boolean isChopped() {
+        return isChopped;
+    }
+
+    @Override
+    public Item getChoppedItem() {
+        this.isChopped = true;
+        this.raw = false;
+        this.name = "Keju Iris";
+        return this;
+    }
+    
+    @Override
+    public void canBePlated() {
+    }
+
+    @Override
+    public boolean isPlated() {
+        return true;
+    }
+
+    @Override
+    public Item getPlatedItem() {
+        return this;
+    }
+}
